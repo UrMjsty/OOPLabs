01293a65baf0e71e39c331ea449ec230c0b9233e
UrMjsty <alexciornyi@gmail.com>
2023-10-03 12:31:39 +0300

    Created FacultyManager

---
+++ Lab2/src/main/java/org/example/processes/FacultyManager.java
@@ -0,0 +1,143 @@
+package org.example.processes;
+
+import org.example.Main;
+import org.example.models.Faculty;
+import org.example.models.StudyField;
+
+import java.util.Scanner;
+
+public class FacultyManager {
+
+    public static String input = "input";
+    public static Scanner scan = new Scanner(System.in);
+    static public void start(){
+        mainMenu();
+    }
+    static private void mainMenu(){
+        System.out.println("Welcome to TUM's student managment system!\n" +
+                "What do you want to do?\n" +
+                "g - General operation \n" +
+                "f - Faculty operations \n" +
+                "s - Student operations");
+        quit();
+        input = scan.nextLine();
+        switch (input){
+            case "q":
+                break;
+            case "g":
+                generalOperations();
+                break;
+            case "f":
+                facultyOperation();
+                break;
+            case "s":
+                studentOperation();
+                break;
+            default:
+                System.out.println("Unknown function, please try again");
+                mainMenu();
+        }
+    }
+    static private void generalOperations(){
+        System.out.println("General Operations\n" +
+                "What do you want to do?\n" +
+                "cr/<faculty name>/<faculty abbreviation>/<field> - create faculty\n" +
+                "ss/<student email> - search and show faculty\n" +
+                "df - display faculties\n" +
+                "df/<field> - display all faculties of a field\n" +
+                "b - Back");
+        quit();
+        input = scan.nextLine();
+        if (input.equals("b")) {
+            mainMenu();
+        }
+        else if (input.equals("q")){
+            return;
+        }
+        int slash = input.indexOf('/');
+        {
+            switch (input.substring(0, 2)){
+                case "cr":
+                    createFaculty(input.substring(3));
+                case "ss":
+                    searchStudent(input.substring(3));
+                    break;
+                case "df":
+                    if (slash == -1)
+                        displayFaculties();
+                    else
+                        displayFaculties(StudyField.valueOf(input.substring(3)));
+                    break;
+                default:
+                    System.out.println("Unknown function, please try again");
+                    generalOperations();
+            }
+        }
+    }
+    static private void facultyOperation(){
+        System.out.println("ns/<faculty abbreviation>/<first name>/<last name>/<email>/<day>/<month>/<year> - create student\n" +
+                "gs/<email> - graduate student\n" +
+                "de/<faculty abbreviation> - display enrolled students\n" +
+                "dg/<faculty abbreviation> - display graduated students\n" +
+                "cb/<faculty abbreviation>/<email> - check if student belongs to faculty\n" +
+                "b - Back ");
+        quit();
+    }
+    static private void studentOperation(){
+        System.out.println("StudOper");
+        doContinue();
+    }
+    static private void createFaculty(String str){
+        System.out.println(str);
+        int slash = str.indexOf('/');
+        String fn = str.substring(0, slash);
+        str = str.substring(slash+1);
+        slash = str.indexOf('/');
+        String fa = str.substring(0, slash);
+        System.out.println(fa);
+        str = str.substring(slash+1);
+        System.out.println(str);
+        StudyField studyField = StudyField.valueOf(str);
+        Faculty faculty = new Faculty(fn, fa, studyField);
+        Main.facultiesList.add(faculty);
+        doContinue();
+    }
+    static private void searchStudent(String str){
+        System.out.println(str);
+        doContinue();
+    }
+    static private void displayFaculties(StudyField studyField){
+        for(Faculty fac: Main.facultiesList)
+            if(fac.getStudyField().equals(studyField))
+                System.out.println(fac.getName());
+        doContinue();
+    }
+    static private void displayFaculties(){
+        //System.out.println("dispFac");
+        for(Faculty fac: Main.facultiesList)
+            System.out.println(fac.getName());
+        doContinue();
+    }
+
+    static private void quit(){
+        System.out.println("\nq - Quit Program\n" +
+                "your input> ");
+    }
+    static private void doContinue(){
+        System.out.println("Continue?\n" +
+                "mm - Back to main menu");
+        quit();
+        input = scan.nextLine();
+        switch (input){
+            case "q":
+                break;
+            case "mm":
+                mainMenu();
+                break;
+            default:
+                System.out.println("Unknown function, please try again");
+                doContinue();
+        }
+
+    }
+}
